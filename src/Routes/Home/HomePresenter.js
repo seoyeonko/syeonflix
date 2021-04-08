0403857a006b0fe7e3817f55928554ab7e837730
seoyeonko <seoyeon99o@naver.com>
2021-04-08 16:55:30 +0900

    presenter structure

--- src/Routes/Home/HomePresenter.js
+++ src/Routes/Home/HomePresenter.js
@@ -1,1 +1,16 @@
-export default () => "Home";
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+
+const HomePresenter = ({ nowPlaying, popular, upcomming, loading, error }) =>
+  null;
+
+HomePresenter.PropTypes = {
+  nowPlaying: PropTypes.array,
+  popular: PropTypes.array,
+  upcomming: PropTypes.array,
+  loading: PropTypes.bool.isRequired,
+  error: PropTypes.string,
+};
+
+export default HomePresenter;
